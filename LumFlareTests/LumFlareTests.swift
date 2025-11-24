6c618c350e9483f08a3b8235a20d9b6336ffe139
Srijan Kyasa <gskyasa@gmail.com>
2025-11-23 20:11:01 -0500

    Initial Commit

---
+++ LumFlareTests/LumFlareTests.swift
@@ -0,0 +1,17 @@
+//
+//  LumFlareTests.swift
+//  LumFlareTests
+//
+//  Created by Srijan Kyasa on 11/23/25.
+//
+
+import Testing
+@testable import LumFlare
+
+struct LumFlareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
